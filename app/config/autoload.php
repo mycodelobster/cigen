67de640f6ff9bdf2c1d868ca6ab8c7011f67a7a5
mycodelobster <mycodelobster@outlook.com>
2014-02-23 18:39:01 +0700

    Update autoload.php

--- app/config/autoload.php
+++ app/config/autoload.php
@@ -1,7 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 $autoload['packages'] = array();
-$autoload['libraries'] = array('database','session','form_validation','sql_helper'');
+$autoload['libraries'] = array('database','session','form_validation','sql_helper');
 $autoload['helper'] = array('url','html','form','text');
 $autoload['config'] = array();
 $autoload['language'] = array();
